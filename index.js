a0881f549c3951a89da592cb76b2a1904f3f786f
Karen Avila <62681407+final-aerith@users.noreply.github.com>
2022-05-24 13:10:41 -0500

    updated JS

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ let gallon = (unitToConvert * 0.2641720524).toFixed(3);
 let liter = (unitToConvert / 0.2641720524).toFixed(3);
 let kilo = (unitToConvert / 2.20462).toFixed(3);
 let pound = (unitToConvert * 2.20462).toFixed(3);
-console.log(unitToConvert + " meters = " + feet + " feet | " + unitToConvert + " feet = " + meter + " meters")
 document.getElementById("length").textContent = unitToConvert + " meters = " + feet + " feet | " + unitToConvert + " feet = " + meter + " meters";
 document.getElementById("volume").textContent = unitToConvert + " liters = " + gallon + " gallons | " + unitToConvert + " gallons = " + liter + " liters";
 document.getElementById("mass").textContent = unitToConvert + " kilos = " + pound + "pounds | " + unitToConvert + " pounds = " + kilo + " kilos";
